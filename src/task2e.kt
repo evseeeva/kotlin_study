057e82b00caa37e0d3ed73a12c87aa0995960e1d
Anastasiya Evseeva <evseeva@surf.dev>
2024-12-01 15:40:53 +0300

    Change variable name

--- src/task2e.kt
+++ src/task2e.kt
@@ -5,8 +5,8 @@ fun main() {
      */
 
     val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val squaredEvenNumbers = numbers.filter { it % 2 == 0 }.map { it * 2 }
-    println(squaredEvenNumbers)
+    val doubleEvenNumbers = numbers.filter { it % 2 == 0 }.map { it * 2 }
+    println(doubleEvenNumbers)
 
     /**
      * Задание 2
